672e011fac3f01df05da3405f8113fafe917867d
Narek <nareksoghomonyan1998@gmail.com>
2023-10-31 21:29:16 +0400

    added class main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wWorld .I am Happy");
+        System.out.println("Hello World.I am Happy");
     }
 }
